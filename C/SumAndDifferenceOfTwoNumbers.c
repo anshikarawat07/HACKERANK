942820318c3929213c1fd1faf16eb8b8aa7685fd
anshikarawat07 <160812154+anshikarawat07@users.noreply.github.com>
2024-08-30 18:33:20 +0530

    Create SumAndDifferenceOfTwoNumbers.c

---
+++ C/SumAndDifferenceOfTwoNumbers.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    float n,m;
+    scanf("%d %d\n",&a,&b);
+    printf("%d %d\n",a+b,a-b);
+    scanf("%f %f\n",&m,&n);
+    printf("%.1f %.1f\n",m+n,m-n);
+    return 0;
+}
