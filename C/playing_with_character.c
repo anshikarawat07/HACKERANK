83d00843d4024c1091f69a6eb9212f09c34030ac
anshikarawat07 <160812154+anshikarawat07@users.noreply.github.com>
2024-08-30 18:35:17 +0530

    Create playing_with_character.c

---
+++ C/playing_with_character.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+   char ch,s[100],sen[100];
+   scanf("%c",&ch);
+   printf("%c\n",ch);
+   scanf("%s",s);
+   printf("%s\n",s);  
+   scanf("\n");
+   gets(sen);
+   puts(sen);
+   return 0;
+}
